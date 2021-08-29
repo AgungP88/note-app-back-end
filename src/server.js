f47dd7fb19d05f8daa86f74b4d3cfe1b1970e8cb
A_Prabowo <agungprabowo8800@gmail.com>
2021-08-29 19:43:55 +0700

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-// const routes = require('./routes');
+const Jwt = require('@hapi/jwt');
+
 // notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
@@ -34,7 +37,29 @@ const init = async () => {
     },
   });
 
-  // server.route(routes);
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
 
   await server.register([
     {
